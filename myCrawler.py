5e035dca25c82c65591b548bf6f977512bad2052
xu chao <xcxcfish@gmail.com>
2017-06-03 20:18:17 +0800

    简单爬虫

---
+++ myCrawler.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python
+#*- coding:utf-8 -*
+#一个简单的爬虫程序 by faithpig
+#2015/06/03
+
+import re
+import urlparse
+import urllib2
+import time
+from datetime import datetime
+import robotparser
+import Queue
+
+'''
+下载网页并且输出
+'''
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def download(url, user_agent='xc'):
+        print 'downloading:', url
+        headers = {'User-agent':user_agent}
+        request = urllib2.Request(url,headers=headers)
+        try:
+            	html = urllib2.urlopen(request).readlines()
+        except urllib2.URLError as e:
+                print 'Download error:', e.reason
+                html = None
+        for line in html:
+                print line
+if __name__ == '__main__':
+        download('http://xcfish.cn/MyBlog/','xc')
