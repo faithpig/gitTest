444ff3f2ee8ec56ce54856312f24f1d552f90a64
xu chao <xcxcfish@gmail.com>
2017-06-03 21:01:13 +0800

    增加爬虫递归爬取<a>标签的功能

--- myCrawler.py
+++ myCrawler.py
@@ -23,11 +23,27 @@ def download(url, user_agent='xc'):
         headers = {'User-agent':user_agent}
         request = urllib2.Request(url,headers=headers)
         try:
-            	html = urllib2.urlopen(request).readlines()
+            	html = urllib2.urlopen(request).read()
         except urllib2.URLError as e:
                 print 'Download error:', e.reason
                 html = None
-        for line in html:
-                print line
+	return html
+
+def link_crawler(start_url, link_pat):
+	'''爬取匹配模式的url页面
+	'''
+	crawl_queue = [start_url]
+	while crawl_queue:
+		url = crawl_queue.pop()
+		html = download(url, user_agent='xc')
+		for link in get_links(html):
+			if re.match(link_pat, link):
+				link = urlparse.urljoin(start_url, link)
+				crawl_queue.append(link)
+def get_links(html):
+	webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE) #这个正则表达式略复杂...匹配了a标签里的链接
+	print webpage_regex.findall(html)
+	return webpage_regex.findall(html)
+
 if __name__ == '__main__':
-        download('http://xcfish.cn/MyBlog/','xc')
+        link_crawler('http://xcfish.cn/MyBlog/','.*/home_viewBlog')
